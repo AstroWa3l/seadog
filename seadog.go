2732305a91a15e1bc63bcae22bcf8ac6a354aace
Wael <wael.t.ivie@gmail.com>
2023-07-31 03:50:33 -0700

    update

--- seadog.go
+++ seadog.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,44 +16,109 @@ func main() {
 
 	// Parse command-line arguments
 	cmd := flag.String("cmd", "", "Command to execute")
-	dataSource := flag.String("url", "", "Source URL")
-	dataType := flag.String("type", "", "ingestion type")
 	flag.Parse()
 
 	// Execute the specified command
 	switch *cmd {
-	case "start-conversation":
+	case "ask":
 		response, err := newConversation(apiKey)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		printResponse(response)
-		// conversationID := response["conversation_id"].(string)
+		// printResponse(response)
 
-	// case "chat":
-	// 	// get the conversation ID
+		// Get the conversation_id from the response
+		conversationID := response["conversation_id"]
 
-	// 	// get the question from the command line
-	// 	question := flag.Arg(1)
-	// 	if question == "" {
-	// 		fmt.Fprintln(os.Stderr, "question is required")
-	// 		os.Exit(1)
-	// 	}
+		// get the question from the command line it will be everything after the
+		// first argument
+		question := os.Args[3:]
+		if len(question) == 0 {
+			fmt.Fprintln(os.Stderr, "question is required")
+			os.Exit(1)
+		}
+		questionString := strings.Join(question, " ")
+		url := "https://api.mendable.ai/v0/mendableChat"
 
-	// 	chatResponse, err := chat(apiKey, question, question , conversationID)
-	// 	if err != nil {
-	// 		fmt.Fprintln(os.Stderr, err)
-	// 		os.Exit(1)
-	// 	}
-	// 	printResponse(chatResponse)
+		data := map[string]interface{}{
+			"api_key":         apiKey,
+			"question":        questionString,
+			"history":         []interface{}{},
+			"conversation_id": conversationID,
+			"shouldStream":    false,
+		}
+
+		payload, err := json.Marshal(data)
+		if err != nil {
+			panic(err)
+		}
+
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+		if err != nil {
+			panic(err)
+		}
+
+		req.Header.Set("Accept", "text/event-stream")
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		// fmt.Println(reflect.TypeOf(body))
+
+		// store the body in a strings
+		bodyString := string(body)
+
+		// fmt.Println(bodyString)
+
+		// get the "answer" from the body
+		answer := strings.Split(bodyString, "answer")
+		// we will get just the "text" from the answer and store it into an array
+		text := strings.Split(answer[1], ":{\"text\":")
+
+		// split the array at the "
+
+		// create a string from the array
+		textString := strings.Join(text, " ")
+
+		// print type of text
+		// fmt.Println(reflect.TypeOf(text))
+
+		// // // print the answer
+		// fmt.Println(textString)
+
+		// remove the " quotation marks from the string
+		textString = strings.ReplaceAll(textString, "\"", "")
+
+		// remove the } from the string
+		textString = strings.ReplaceAll(textString, "}", "")
+
+		// drop/replace everything after the "soources" in the string (i have no clue wtf I am doin XD)
+		textString = strings.Split(textString, ",sources")[0]
+
+		// print the answer
+
+		fmt.Println(textString)
 
 	case "ingest":
-		if *dataSource == "" || *dataType == "" {
+		dataSource := os.Args[3]
+		dataType := os.Args[4]
+
+		if dataSource == "" || dataType == "" {
 			fmt.Fprintln(os.Stderr, "url and Type are required")
 			os.Exit(1)
 		}
-		response, err := ingestData(apiKey, *dataSource, *dataType)
+		response, err := ingestData(apiKey, dataSource, dataType)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -124,7 +190,7 @@ func ingestData(apiKey string, dataSource string, dataType string) (map[string]i
 	return response, nil
 }
 
-func chat(apiKey string, question string, history []map[string]string, conversationID int) (string, error) {
+func chat(apiKey string, question string, history []map[string]string, conversationID float64) (string, error) {
 	url := "https://api.mendable.ai/v0/mendableChat"
 
 	data := map[string]interface{}{
@@ -132,6 +198,7 @@ func chat(apiKey string, question string, history []map[string]string, conversat
 		"question":        question,
 		"history":         history,
 		"conversation_id": conversationID,
+		// "shouldStream":    false,
 	}
 
 	jsonData, err := json.Marshal(data)
@@ -144,7 +211,7 @@ func chat(apiKey string, question string, history []map[string]string, conversat
 		return "", err
 	}
 
-	req.Header.Set("Accept", "text/event-stream")
+	// req.Header.Set("Accept", "text/event-stream")
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
@@ -171,150 +238,3 @@ func printResponse(response map[string]interface{}) {
 	}
 	fmt.Println(string(responseJSON))
 }
-
-// package main
-
-// import (
-// 	"bytes"
-// 	"encoding/json"
-// 	"fmt"
-// 	"io/ioutil"
-// 	"net/http"
-// )
-
-// func main() {
-// 	apiKey := "d67542ec-87c7-489e-8c66-24895eb57136"
-
-// 	// Send a newConversation request
-// 	newConversationRequestBody := map[string]interface{}{
-// 		"api_key": apiKey,
-// 	}
-// 	newConversationResponse, err := newConversation(apiKey, "newConversation", newConversationRequestBody)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	// convert the response map into a JSON string and print the response
-// 	newConversationResponseJSON, err := json.Marshal(newConversationResponse)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	fmt.Println(string(newConversationResponseJSON))
-
-// 	// Now let's ingest some data
-// 	ingestResponse, err := ingestData(apiKey, "https://api.mendable.ai/v0/ingestData", "url")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	// convert the response map into a JSON string and print the response
-// 	ingestResponseJSON, err := json.Marshal(ingestResponse)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	fmt.Println(string(ingestResponseJSON))
-
-// 	// Get the task ID from the ingest response
-// 	taskID := ingestResponse["task_id"].(string)
-
-// 	// Now let's check the status of the task
-// 	ingestErr := fetchIngestionStatus(taskID)
-// 	if ingestErr != nil {
-// 		fmt.Println(ingestErr)
-// 	}
-
-// }
-
-// func newConversation(apiKey string, endpoint string, requestBody map[string]interface{}) (map[string]interface{}, error) {
-// 	url := fmt.Sprintf("https://api.mendable.ai/v0/%s", endpoint)
-
-// 	// Create the request body
-// 	requestBodyBytes, err := json.Marshal(requestBody)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// Send the HTTP request
-// 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(requestBodyBytes))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer resp.Body.Close()
-
-// 	// Read the response body
-// 	var response map[string]interface{}
-// 	err = json.NewDecoder(resp.Body).Decode(&response)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return response, nil
-// }
-
-// func ingestData(apiKey string, url string, ingestionType string) (map[string]interface{}, error) {
-// 	// Create the request body
-// 	requestBody := map[string]string{
-// 		"api_key": apiKey,
-// 		"url":     url,
-// 		"type":    ingestionType,
-// 	}
-// 	requestBodyBytes, err := json.Marshal(requestBody)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	// Send the HTTP request
-// 	resp, err := http.Post("https://api.mendable.ai/v0/ingestData", "application/json", bytes.NewBuffer(requestBodyBytes))
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer resp.Body.Close()
-
-// 	// Read the response body
-// 	var response map[string]interface{}
-// 	err = json.NewDecoder(resp.Body).Decode(&response)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return response, nil
-// }
-
-// func fetchIngestionStatus(taskID string) error {
-// 	url := "https://api.mendable.ai/v0/ingestionStatus"
-
-// 	data := map[string]string{
-// 		"task_id": taskID,
-// 	}
-
-// 	jsonData, err := json.Marshal(data)
-// 	if err != nil {
-// 		return fmt.Errorf("error marshalling JSON: %v", err)
-// 	}
-
-// 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-// 	if err != nil {
-// 		return fmt.Errorf("error creating request: %v", err)
-// 	}
-
-// 	req.Header.Set("Content-Type", "application/json")
-
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		return fmt.Errorf("error sending request: %v", err)
-// 	}
-
-// 	defer resp.Body.Close()
-
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return fmt.Errorf("error reading response body: %v", err)
-// 	}
-
-// 	fmt.Println(string(body))
-
-// 	return nil
-// }
